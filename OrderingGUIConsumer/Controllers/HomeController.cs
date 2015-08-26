76be3f3d1738e7400f209faa17cea7c66262a111
Arthick <arthick.sundaresan@gmail.com>
2015-08-26 17:26:12 +0530

    Clean Code

--- OrderingGUIConsumer/Controllers/HomeController.cs
+++ OrderingGUIConsumer/Controllers/HomeController.cs
@@ -16,20 +16,5 @@ namespace OrderingGUIConsumer.Controllers
         {
             return View();
         }
-
-
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
